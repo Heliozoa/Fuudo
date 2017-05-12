e4f5a892327468418f6f5b735e2b636a9962db1a
Heliozoa <dm89132@gmail.com>
2017-05-12 17:56:09 +0300

    Muutettu routeja

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var foodDAO = require('../models/mockFoodDAO');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Fuudo',
+                        food: foodDAO.getAny()});
 });
 
 module.exports = router;
